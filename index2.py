c28465ee83c4b470d82432d5016a6d306c928b2c
Sagar Tamang <sagar.bdr0000@gmail.com>
2023-04-30 09:21:23 +0530

    later commit

--- index2.py
+++ index2.py
@@ -1,6 +1,7 @@
+# This Shows A Normal Chat Messages From ChatGPT
+
 import openai
 openai.api_key = "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"
-
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
@@ -13,11 +14,11 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     )
     return response.choices[0].message["content"]
 
-text_1 = f"""
-"""
-prompt = f"""
-What is India's capital?
-"""
-response = get_completion(prompt)
-print("Completion for Text 1:")
-print(response)
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+#     print(str(response.choices[0].message))
+    return response.choices[0].message["content"]
